6bd30b27d54cf9a065600bbee63f4004a709db30
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-30 17:45:58 -0300

    funções

--- comparacao.js
+++ comparacao.js
@@ -2,8 +2,8 @@
 var numero = 12
 const texto = "12"
 // dois iguais é uma pergunta de conteudo sem tipagem
-console.log(texto == numero);
+// console.log(texto == numero);
 // tres iguais verifica se o conteudo é o mesmo e também checa a tipagem
-console.log(texto === numero);
+// console.log(texto === numero);
 // dois iguais e uma esclamação checam se é diferente o conteudo
-console.log(texto !== numero);
+// console.log(texto !== numero);
