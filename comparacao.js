1ed61703e27b591871d4aaf77b2dce3ac6477303
JulioVianaDev <JulioVianaDev@gmail.com>
2023-03-30 17:21:04 -0300

    comparações das variaveis

---
+++ comparacao.js
@@ -0,0 +1,9 @@
+// um igual  significa atribuição
+var numero = 12
+const texto = "12"
+// dois iguais é uma pergunta de conteudo sem tipagem
+console.log(texto == numero);
+// tres iguais verifica se o conteudo é o mesmo e também checa a tipagem
+console.log(texto === numero);
+// dois iguais e uma esclamação checam se é diferente o conteudo
+console.log(texto !== numero);
